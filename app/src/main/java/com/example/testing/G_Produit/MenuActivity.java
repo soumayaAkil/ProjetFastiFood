d0d18d43dc4bd4fac9751b3ea0ed5f6b5d84c0d0
soumayaAkil <85191150+soumayaAkil@users.noreply.github.com>
2022-04-29 16:52:10 +0100

    FirstCommit

---
+++ app/src/main/java/com/example/testing/G_Produit/MenuActivity.java
@@ -0,0 +1,50 @@
+package com.example.testing.G_Produit;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.example.testing.AcceuilFragment;
+import com.example.testing.CommandeFragment;
+import com.example.testing.FavoriesFragment;
+import com.example.testing.Profile.ProfileFragment;
+import com.example.testing.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MenuActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigationView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+        bottomNavigationView=(BottomNavigationView) findViewById(R.id.bottomnavigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(naviglistener);
+    }
+    public BottomNavigationView.OnNavigationItemSelectedListener naviglistener=new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+            Fragment unFragment=null;
+            switch (menuItem.getItemId())
+            {
+                case R.id.Acceuil:
+                    unFragment= new AcceuilFragment();
+                    break;
+                case R.id.Commande:
+                    unFragment= new CommandeFragment();
+                    break;
+                case R.id.Recherche:
+                    unFragment= new FavoriesFragment();
+                    break;
+                case R.id.Profile:
+                    unFragment= new ProfileFragment();
+                    break;
+            }
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragmentplaceholder,unFragment).commit();
+            return  true;
+        }
+    };
+}
